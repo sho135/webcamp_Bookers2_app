256c76bd57d19a4d13897349e8ba5cab16801e8c
sho135 <sh135pro@gmail.com>
2021-09-25 07:57:20 +0000

    add-2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def new
+      @book = Book.new
+  end
+
+  def create
+      @book = Book.new(book_params)
+      @book.user_id = current_user.id
+      @book.save
+      redirect_to book_path
+  end
+
+  def index
+      @book = book.all
+  end
+
+  def show
+      @book = book.find(params[:id])
+  end
+
+  def destroy
+      @book = book.find(params[:id])
+      @book.destroy
+      redirect_to book_path
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_003909) do
+ActiveRecord::Schema.define(version: 2021_09_25_043215) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "opinion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
