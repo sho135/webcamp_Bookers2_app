8156b4d06f62bf10f9691e0fe7ca5fa39d9690bf
sho135 <sh135pro@gmail.com>
2021-09-27 04:54:12 +0000

    add-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show]
+  root 'homes#top'
+
+  resources :books
+  resources :users
 end
