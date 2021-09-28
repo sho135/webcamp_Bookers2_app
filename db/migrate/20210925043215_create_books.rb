94400365eb0fb923d77fe5d864079f0464f2e64a
sho135 <sh135pro@gmail.com>
2021-09-28 11:56:45 +0000

    add-7

--- db/migrate/20210925043215_create_books.rb
+++ db/migrate/20210925043215_create_books.rb
@@ -2,10 +2,11 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
 
-      t.text :title
+      t.string :title
       t.text :body
       t.integer :user_id
       t.integer :profile_image_id
+      t.integer :book_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,9 @@ ActiveRecord::Schema.define(version: 2021_09_25_043215) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "opinion"
+    t.text "body"
+    t.integer "user_id"
+    t.integer "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
