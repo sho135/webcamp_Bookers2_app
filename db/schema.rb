0b08dfa2b51ee7d8bc9fb69b743d0f0307bb8d94
sho135 <sh135pro@gmail.com>
2021-10-02 12:49:42 +0000

    add-17

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_104023) do
+ActiveRecord::Schema.define(version: 2021_10_02_123023) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_10_02_104023) do
     t.datetime "updated_at", null: false
     t.string "introduction"
     t.string "profile_image_id"
-    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
