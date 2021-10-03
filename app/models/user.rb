fd9e6d68994a8f83c917e1be8b7ac9cc2cd40bca
sho135 <sh135pro@gmail.com>
2021-10-03 12:41:58 +0000

    add-20

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: {in: 2..20}
+  validates :name, uniqueness: true, length: {in: 2..20}
   validates :introduction, length: {maximum: 50}
 
   has_many :books, dependent: :destroy
