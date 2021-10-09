37da9177671e83b32283b6c93ee0b50e071633d5
sho135 <sh135pro@gmail.com>
2021-10-09 10:06:57 +0000

    edit: errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,16 +15,13 @@ class UsersController < ApplicationController
 
     def edit
         @user = User.find(params[:id])
-        if @user == current_user
-            render "edit"
-        else
+        if @user != current_user
             redirect_to user_path(current_user)
         end
     end
 
     def update
         @user = User.find(params[:id])
-        byebug
         if @user.update(user_params)
             flash[:notice]="You have updated user successfully."
             redirect_to user_path(@user.id)
